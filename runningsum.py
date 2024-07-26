40faa6ed9c2982ec472155e4ee3de717b4b0c744
Buhay Malaya <malayabuhay@gmail.com>
2024-07-26 11:59:58 -0400

    pseudo

--- runningsum.py
+++ runningsum.py
@@ -5,3 +5,8 @@
 # Additionally, if the number is negative, return 0.
 
 # Note: If the number is a multiple of both 3 and 5, only count it once.
+
+# maybe find the range until number
+# start at 3 and increment by 1, stop at number
+# if number is divisible by 3 and 5, add it to running sum once
+# sum = 0
