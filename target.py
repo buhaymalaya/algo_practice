45e85e86f1457ead972468f5cafcf81cdc617b87
Buhay Malaya <malayabuhay@gmail.com>
2024-08-01 18:46:35 -0400

    try

--- target.py
+++ target.py
@@ -46,4 +46,8 @@ def two_sum(nums, target):
     
     for index, num in enumerate(nums):
         complement = target - num
-        
+        if complement in num_to_index:
+            return [num_to_index[complement], index]
+        num_to_index[num] = index
+    
+    
